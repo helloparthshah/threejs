8f36bce805f6096af698b1180a187e137d44245f
Parth Shah <helloparthshah@gmail.com>
2022-02-11 19:25:26 -0800

    changed the color palette

--- src/App.js
+++ src/App.js
@@ -12,32 +12,41 @@ import {
 } from '@react-three/drei'
 import "./App.css"
 
-function Plane({hoverable=false, color = "white", ...props }) {
-  const [hovered, set] = useState(false)
-  const [lcolor, setColor] = useState(color)
-  useEffect(() => {
-    // Split the color into h,s and l
-    const hsl = color.match(/hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/)
-    const h = parseInt(hsl[1])
-    const s = parseInt(hsl[2])
-    const l = parseInt(hsl[3])
-    setColor(`hsl(${h}, ${s}%, ${Math.floor(l * (hovered ? 1.05 : 1))}%)`)
-  }, [color, hovered])
+function Plane({color = "white", ...props }) {
   return (
-    <mesh {...props} onPointerOver={(e) => {
-      if (hoverable) {
+    <mesh {...props}>
+      <planeGeometry />
+      <meshBasicMaterial color={color} />
+    </mesh>
+  )
+}
+
+function Project({
+  isHover = false,
+  number = 0,
+  ...props
+}) {
+  const { width, height } = useThree((state) => state.viewport)
+  const [hovered, set] = useState(false)
+  const color = "hsl(267, 68%, 55%)"
+  const lcolor = "hsl(267, 68%, 60%)"
+  const size = [width / 2, width / 3.5, 1]
+  const data = useScroll()
+  const ref= useRef()
+  useFrame(() => {
+    ref.current.children[0].rotation.z = (number%2===0?-1:1)*data.range(0, Math.PI)
+  })
+  return (
+    <group ref={ref}>
+    <Plane {...props} position={[(number%2===0?-1:1)*width/6, 1+number*-height, 0]} 
+        color={hovered?lcolor:color} scale={size} onPointerOver={(e) => {
         document.body.style.cursor = "pointer";
         set(true)
-      }
       }} onPointerOut={(e) => {
-      if (hoverable) {
         document.body.style.cursor = "auto";
         set(false)
-      }
-        }}>
-      <planeGeometry />
-      <meshBasicMaterial color={lcolor} />
-    </mesh>
+        }}/>
+        </group>
   )
 }
 
@@ -45,22 +54,16 @@ function Page({...props}) {
   const { width, height } = useThree((state) => state.viewport)
   const data = useScroll()
   const group = useRef()
-  const img = useRef()
-  const size = [width / 1.75, width / 1.75 / 1.75, 1]
   useFrame(() => {
-    group.current.children[0].rotation.z = data.range(0, Math.PI)
-    group.current.children[2].rotation.z = -data.range(0, Math.PI)
-    group.current.children[3].rotation.z = data.range(0, Math.PI)
-    group.current.children[1].position.y = 1 + data.range(0, 1 / 3)
-    group.current.children[4].position.y = -data.offset * height * 5 + height*1.3
+    group.current.children[3].position.y = -data.offset * height * 5 + height*1.3
   })
   return (
     <>
       <group ref={group}>
-        <Plane scale={size} color="hsl(139, 38%, 82%)" position={[0, 1, 0]} hoverable={true}/>
-        <Plane scale={size} color="hsl(139, 38%, 82%)" position={[0, -height+1,0]}/>
-        <Plane scale={size} color="hsl(139, 38%, 82%)" position={[0, -height*2+1,0]}/>
-        <Plane scale={[100, width/2, 1]} rotation={[0, 0, Math.PI / 4]} position={[0, height, -1]} color="hsl(177, 51%, 93%)" />
+        <Project number={0}/>
+        <Project number={1}/>
+        <Project number={2}/>
+        <Plane scale={[100, width/2, 1]} rotation={[0, 0, Math.PI / 4]} position={[0, height, -1]} color="#131313" />
       </group>
     </>
   )
