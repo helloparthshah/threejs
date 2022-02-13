6cb327c6d1bd6f6d90dc59063be915f7562914bd
Parth Shah <helloparthshah@gmail.com>
2022-02-12 20:27:23 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -27,19 +27,30 @@ function Project({
   number = 0,
   ...props
 }) {
-  const { width, height } = useThree((state) => state.viewport)
+  let { width, height } = useThree((state) => state.viewport)
+  const [h, setH] = useState(height)
+  const [w, setW] = useState(width)
   const [hovered, set] = useState(false)
-  const color = "hsl(267, 68%, 55%)"
-  const lcolor = "hsl(267, 68%, 60%)"
-  const size = [width / 2, width / 3.5, 1]
+  const color = "hsl(41, 87%, 52%)"
+  const lcolor = "hsl(41, 87%, 58%)"
+  const size = [w / 2, w / 3.5, 1]
   const data = useScroll()
   const ref= useRef()
+  useEffect(() => {
+    if (width <= 10) {
+      state.pages = 10 * state.pages / height
+      setH(10)
+      console.log(state.pages)
+    }
+  }, [height, width])
   useFrame(() => {
-    ref.current.children[0].rotation.z = (number % 2 === 0 ? -1 : 1) * data.range((number) / state.pages, (number + 1) / state.pages) * 1 / 3
+    console.log(w)
+    let rot = (number % 2 === 0 ? -1 : 1) * data.range((number) / state.pages, (number + 1) / state.pages) * 1 / 3
+    ref.current.children[0].rotation.z = rot
   })
   return (
     <group ref={ref}>
-    <Plane {...props} position={[(number%2===0?-1:1)*width/5, 1+number*-height, 0]} 
+    <Plane {...props} position={[(number%2===0?-1:1)*w/5, 1+number*-h, 0]} 
         color={hovered?lcolor:color} scale={size} onPointerOver={(e) => {
         document.body.style.cursor = "pointer";
         set(true)
@@ -47,7 +58,7 @@ function Project({
         document.body.style.cursor = "auto";
         set(false)
         }}/>
-        <Html width={width/2} position={[(number%2===0?1:-1)*width/10,height/state.pages+number*-height,0]}>
+        <Html width={w/2} position={[(number%2===0?1:-1)*w/10,h/state.pages+number*-h,0]}>
           hello world!
         </Html>
         </group>
@@ -58,16 +69,29 @@ function Page({...props}) {
   const { width, height } = useThree((state) => state.viewport)
   const data = useScroll()
   const group = useRef()
+  const html = useRef()
+  const stripeWidth = height 
   useFrame(() => {
-    group.current.children[3].position.y = -data.offset * height * (state.pages+2) + height*(100+state.pages*10)/100
+    group.current.children[3].position.y = -data.offset * height * (state.pages + 2) + stripeWidth
+  })
+  useFrame(({
+    mouse
+  }) => {
+    let x = THREE.MathUtils.lerp(html.current.style.top.replace("px", ""), (mouse.y - data.offset) * height * 5, 0.1)
+    let y = THREE.MathUtils.lerp(html.current.style.left.replace("px", ""), -mouse.x * width * 5, 0.1)
+    html.current.style.top = `${x}px`
+    html.current.style.left = `${y}px`
   })
   return (
     <>
-      <group ref={group}>
+      <group ref={group} {...props}>
         <Project number={1}/>
         <Project number={2}/>
         <Project number={3}/>
-        <Plane scale={[100, width/2, 1]} rotation={[0, 0, Math.PI / 4]} position={[0, height, -1]} color="#131313" />
+        <Plane scale={[100, stripeWidth, 1]} rotation={[0, 0, Math.PI / 4]} position={[0, 0, -1]} color="#131313" />
+        <Html ref={html} center wrapperClass="html">
+            <h1 id={"name"} style={{fontSize:"6em"}}>Parth Shah</h1>
+        </Html>
       </group>
     </>
   )
@@ -80,11 +104,6 @@ function App() {
         <ScrollControls pages={state.pages} damping={4}>
           <Scroll>
             <Page />
-          </Scroll>
-          <Scroll html>
-            <div style={{width:"100vw",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
-              <h1 style={{fontSize:"6em"}}>Parth Shah</h1>
-            </div>
           </Scroll>
         </ScrollControls>
       </Canvas>
